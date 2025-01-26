aba5a077459786308b6813315ef35c0757cb3700
dshuais <1137896420@qq.com>
2025-01-26 12:24:31 +0800

    ✨ feat: 优化类型提示

--- packages/turbo-utils/typings.d.ts
+++ packages/turbo-utils/typings.d.ts
@@ -2,10 +2,10 @@
  * @Author: dushuai
  * @Date: 2023-12-31 09:33:25
  * @LastEditors: dushuai
- * @LastEditTime: 2025-01-26 12:21:59
+ * @LastEditTime: 2025-01-26 12:23:50
  * @description: ts类型文件
  */
 
-export type Rules<T extends Empty<string>> = { valid?: boolean; key: keyof T; msg: string; };
+export type Rules<T = Empty<string>> = { valid?: boolean; key: keyof T; msg: string; };
 
 export type Empty<K extends string | number | symbol = string, T = any> = Partial<Record<K, T>>;
